4ef0c574ecc35903ebc105220dc5cf3e212d8986
Farah <farah.hsh.12@gmail.com>
2023-08-14 16:14:49 +0300

    day6

---
+++ main.js
@@ -0,0 +1,84 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day6
+
+// operater conatin assignment, logical, arthemtic, comparison, compound assignment, string
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// arthemtic  operater contain /   +   -   %   **
+const x =5;
+const y =10;
+
+console.log("addition: ", x + y);
+console.log("subtraction: ", x - y);
+console.log("division: ", x / y);
+console.log("multiplication: ", x * y);
+console.log("division remainder: ", x % y);
+console.log("exponent: ", x ** y);
+
+let n1=10;
+let n2=10;
+// add by 1
+console.log(n1);
+console.log(n1++);
+console.log(n1);
+// immedietly add 1 without using print again after
+console.log(++n1);
+// subtract by 1
+console.log(n2--);
+console.log(n2);
+// subtract by 1 immedietly without using print again after
+console.log(--n2);
+
+// end
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// compound assignment operater
+
+let c=10;
+c+=2;
+console.log(c);
+
+c-=2;
+console.log(c);
+
+c*=2;
+console.log(c);
+
+c/=2;
+console.log(c);
+
+c**=2;
+console.log(c);
+
+c%=2;
+console.log(c);
+
+// end
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// string operater
+let greeting= "hi";
+console.log(greeting + " " + "alice");
+let sentence= "happy new year";
+let newsentence= sentence + 2024;
+console.log(newsentence);
+
+
+let sentences= "Happy New ";
+sentences += "Year";
+console.log(sentences);
+
+
+
+
+
+
+
+
+
+
+
+
+
